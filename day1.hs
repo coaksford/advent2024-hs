9275c711ac3bebf725de8345f8bcdfe34cb6e78b
coaksford <39574029+coaksford@users.noreply.github.com>
2024-12-01 01:23:10 -0600

    Day 1: Got splitting and parsing to work

--- day1.hs
+++ day1.hs
@@ -1,4 +1,12 @@
 main = do
   contents <- readFile "day1input1.txt"
   print contents
+  print $ fmap parseRow $ fmap words $ lines contents
+
+parseRow :: [String] -> (Int, Int)
+parseRow (first:second:_) = (read first, read second)
+parseRow (_) = (0,0)
+
+getDistance :: (Int, Int) -> Int
+getDistance (x, y) = abs $ y - x
 
