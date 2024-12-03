4c93101a321ea414259eb986b484f83259767b62
coaksford <39574029+coaksford@users.noreply.github.com>
2024-12-03 02:05:37 -0600

    Day 3: Part 2 working. Went pretty smoothly this time!

--- day3.hs
+++ day3.hs
@@ -6,6 +6,11 @@ part1 = do
   contents <- readFile "input3.txt"
   let total = findMatch contents
   print total
+
+part2 = do
+  contents <- readFile "input3.txt"
+  let total = findMatchModal True contents
+  print total
    
 findMatch :: String -> Integer
 findMatch "" = 0 
@@ -22,4 +27,31 @@ findMatch input = if (take 4 input) == "mul("
                     then (num1 * num2) + findMatch rest3
                     else findMatch $ tail input
                   else findMatch $ tail input
+
+findMatchModal :: Bool -> String -> Integer
+findMatchModal _ "" =  0
+findMatchModal active input =
+  if active
+  then
+    if (take 7 input) == "don't()"
+    then
+      findMatchModal False $ drop 7 input
+    else
+      let rest = drop 4 input
+      in if (take 4 input) == "mul("
+        then
+          let
+            rest1 = drop 4 input
+            num1 = read (takeWhile isDigit rest1) :: Integer
+            (hopefullyComma:rest2) = (dropWhile isDigit rest1)
+            num2 = read (takeWhile isDigit rest2) :: Integer
+            (hopefullyParen:rest3) = (dropWhile isDigit rest2)
+          in
+            if hopefullyParen == ')' && hopefullyComma == ',' && num1 < 1000 && num1 > -1 && num2 < 1000 && num2 > -1
+            then (num1 * num2) + findMatchModal active rest3
+            else findMatchModal active $ tail input
+        else findMatchModal active $ tail input
+  else if (take 4 input) == "do()"
+    then findMatchModal True $ drop 4 input
+    else findMatchModal active $ tail input
 
