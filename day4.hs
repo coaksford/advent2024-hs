e2c7e14dabd7dfc6f9b2fdcd4f8baa857ec603f1
coaksford <39574029+coaksford@users.noreply.github.com>
2024-12-04 11:40:51 -0600

    Day 4: Part 2 working. Was more troublesome than expected!
    Originnally, isXofMAS took one 3x3 list and reversed and transposed it
    into all four orientations to see if it would match, but for some
    reason, known matching inputs weren't "matching" the pattern. I
    eventually got it working by just repeating the pattern 4 times but it
    feels more ugly this way.

--- day4.hs
+++ day4.hs
@@ -16,6 +16,42 @@ part1 = do
   let searchArea = lines contents
   print $ search searchArea
 
+part2 = do
+  contents <- readFile "input4.txt"
+  let searchArea = lines contents
+  print $ searchX searchArea
+
+crop = take 3
+
+searchX :: [String] -> Int
+searchX [] = 0
+searchX input =
+  let
+    band = crop input
+    next = tail input
+  in
+  searchBandForX band + if length next >= 3 then searchX next else 0
+
+searchBandForX :: [String] -> Int
+searchBandForX [] = 0
+searchBandForX [_,_,[]] = 0
+searchBandForX [_,[],_] = 0
+searchBandForX [[],_,_] = 0
+searchBandForX input =
+  let
+    band = crop input
+    square = fmap crop band
+    restOfBand = searchBandForX (fmap tail band)
+  in
+  (if isXofMAS square then 1 else 0) + restOfBand
+
+isXofMAS :: [String] -> Bool
+isXofMAS (('M':_:'M':_):(_:'A':_):('S':_:'S':_):_) = True
+isXofMAS (('S':_:'S':_):(_:'A':_):('M':_:'M':_):_) = True
+isXofMAS (('M':_:'S':_):(_:'A':_):('M':_:'S':_):_) = True
+isXofMAS (('S':_:'M':_):(_:'A':_):('S':_:'M':_):_) = True
+isXofMAS _ = False
+
 search :: [String] -> Int
 search input =
   sum (fmap searchHorizontal input) -- forward
